14289a399eba397bd96bf82f3444b1c4ed4aa4d5
RitikShah <ritik10.shah@gmail.com>
2025-07-01 23:19:39 -0700

    fix: refactor item abstractions to be more streamlined

--- src/lib/item.py
+++ src/lib/item.py
@@ -2,18 +2,18 @@ import copy
 from inspect import signature, Signature
 import json
 from typing import Any, ClassVar
-from abc import ABC
+
 
 def title_case_to_snake_case(title_case_str):
     snake_case_str = []
     for index, char in enumerate(title_case_str):
         # TitleCase usually starts with an uppercase so we ignore the first character
         if char.isupper() and index != 0:
-            snake_case_str.append('_')
-        
+            snake_case_str.append("_")
+
         snake_case_str.append(char.lower())
 
-    return ''.join(snake_case_str)
+    return "".join(snake_case_str)
 
 
 def nbt_dump(obj: dict[str, Any]):
@@ -37,8 +37,8 @@ def nbt_dump(obj: dict[str, Any]):
     return serialize(obj)
 
 
-
 class ItemError(Exception): ...
+
 
 class ComponentError(ItemError):
     def __init__(self, name: str, component: Any, signature: Signature):
@@ -54,8 +54,28 @@ class ComponentError(ItemError):
 
 
 class ItemMeta(type):
+    """Metaclass for item classes.
+    
+    This essentially works as a custom type for all items. Since we do not instantiate items,
+    we use metaclasses to effectively turn class definitions into objects. This allows us
+    to define items and their components in a more Pythonic way, while still allowing us
+    to dynamically define behavior.
+
+    The metaclass only activates on usage of the `.components` property. This triggers
+    a search of all of the members on the class, deciphering whether the member is a 
+    component or some custom logic, applying all of our changes, and then returning
+    a vanilla set of components with various amounts of metadata.
+    
+    The custom logic defined are class methods with a name that begins with either
+    - "{name}_component"
+    - "{name}_transformer"
+
+    These act as modifiers that either return a new set of components or a modified value
+    for a component that lets us define a lot of custom features (see `item` for full usage).
+    """
+
     registered_items: ClassVar[dict[str, "ItemMeta"]] = {}
-    _components: dict[str, Any]
+    _namespace: dict[str, Any]
 
     def __new__(cls, name: str, bases: list[type], namespace: dict[str, Any]):
         if name == "item":
@@ -66,110 +86,94 @@ class ItemMeta(type):
                 f"item '{name}' should be defined with `snake_case` "
                 f"('{title_case_to_snake_case(name)}')."
             )
-        
-        if len(bases) > 1:
-            raise ItemError(
-                "item classes do not support multiple inheritance\n"
-                f"`{name}`: `{bases}`"
-            )
 
-        prev_dict = {k: v for k, v in bases[0].__dict__.items() if not k.startswith("__")}
-        namespace = cls.prep_namespace(name, deep_merge_dicts(prev_dict, namespace))
-
-        cls.registered_items[name] = new_cls = super().__new__(cls, name, bases, namespace)
+        cls.registered_items[name] = new_cls = super().__new__(
+            cls, name, bases, namespace
+        )
         return new_cls
-    
-    @classmethod
-    def prep_namespace(cls, name: str, namespace: dict[str, Any]):
-        namespace.setdefault("custom_data", {})
-        namespace["custom_data"].setdefault("item_ids", [])
-        namespace.setdefault("_custom_components", [])
-        namespace["custom_data"].setdefault("custom_components", {})
-        namespace["custom_data"]["item_id"] = name
-        namespace["custom_data"]["item_ids"].append(name)
-        
-        namespace = cls.resolve_callables(namespace)
-        namespace["_components"] = cls.calculate_components(namespace)
-        return namespace
-    
+
     @property
     def components(self):
-        return self._components
+        output_components = {}
+        callable_members = {}
+
+        # Split namespace into callable and non-callable members
+        for member in dir(self):
+            if not member.startswith("_") and (val := getattr(self, member)) is not None:
+                if callable(val):
+                    callable_members[member] = val
+                else:
+                    output_components[member] = copy.deepcopy(val)
+
+        # inject custom data
+        output_components = deep_merge_dicts(output_components, {"custom_data": {"item_id": self.name}})
+
+        # For each callable member, handle component and transformer
+        for k, func in callable_members.items():
+
+            # Custom components return a dictionary containing one or more components
+            if (index := k.find("_component")) > 1:
+                name = k[:index]
+                sig = signature(func)
+
+                # If the component is defined in the item, apply the function
+                if component := output_components.get(name):
+                    try:
+                        # Call the custom component function based on the signature 
+                        if type(component) is not dict:
+                            if len(sig.parameters) > 1:
+                                raise ComponentError(name, component, sig)
+                            new_namespace = func(component)
+                        else:
+                            new_namespace = func(**component)
+
+                        # If our result exists, deeply merge our new components
+                        # We also inject some metadata
+                        if new_namespace is not None:
+                            metadata = {
+                                "custom_components": {name: component},
+                                "_custom_components": [name]
+                            }
+                            output_components["custom_data"] = deep_merge_dicts(output_components["custom_data"], metadata)
+                            
+                            output_components = deep_merge_dicts(output_components, new_namespace)
+
+                    except Exception as e:
+                        raise ComponentError(name, component, sig) from e
+
+            # Transformers modify the value of an existing component
+            elif (index := k.find("_transformer")) > 1:
+                name = k[:index]
+                
+                # If the component exists, apply the transformer function
+                if name in output_components:
+                    if (value := func(output_components[name])) is not None:
+                        output_components[name] = value
+
+        return output_components
     
     @property
     def name(self):
         return self.__name__
-    
+
     @property
     def has_id(self):
         return "id" in self.__dict__
-    
+
     def item_string(self):
         if not self.has_id:
             raise ItemError(f"`{self.name}` item must define an `id`!")
-        
+
         components = ",".join(f"{k}={nbt_dump(v)}" for k, v in self.components.items())
         return f"{self.id}[{components}]"
-    
+
     def conditional_string(self):
         id = self.__dict__.get("id", "*")
         return f"{id}[custom_data~{{item_ids:['{self.name}']}}]"
-    
+
     __neg__ = __invert__ = conditional_string
-    __pos__ = __str__  = item_string
+    __pos__ = __str__ = item_string
 
-    @classmethod
-    def calculate_components(cls, namespace: dict[str, Any]):
-        return {
-            k: v
-            for k, v in namespace.items()
-            if (
-                not k.startswith("_") 
-                and not callable(v)
-                and k not in ["id", "minecraft:id"]
-                and k not in namespace.get("_custom_components", [])
-                and v is not drop
-            )
-        }
-    
-    @classmethod
-    def resolve_callables(cls, namespace: dict[str, Any]):
-        for k, func in namespace.items():
-            if not callable(func):
-                continue
-            
-            if (index := k.find("_component")) > 1:
-                name = k[:index]
-                sig = signature(func)
-                
-                if component := namespace.get(name):
-                    if type(component) is not dict:
-                        if len(sig.parameters) > 1:
-                            raise ComponentError(name, component, sig)
-                        
-                        new_namespace = func(component)
-                    
-                    else:
-                        new_namespace = func(**component)
-                    
-                    if new_namespace is not None:
-                        namespace["custom_data"]["custom_components"] = deep_merge_dicts(
-                            namespace["custom_data"]["custom_components"],
-                            {name: component}
-                        )
-                        namespace = deep_merge_dicts(namespace, new_namespace)
-                        namespace["_custom_components"].append(name)
-
-            elif (index := k.find("_transformer")) > 1:
-                name = k[:index]
-
-                if name in namespace:
-                    if (value := func(namespace[name])) is not None:
-                        namespace[name] = value
-
-        return namespace
-    
-                
     def __call__(*_, **__):
         raise ItemError("`item` classes cannot be instantiated, see docstring.")
 
@@ -186,7 +190,7 @@ def deep_merge_dicts(d1: dict[str, Any], d2: dict[str, Any]) -> dict[str, Any] |
     """
 
     merged = copy.deepcopy(dict(d1))  # Make a copy of the first dictionary
-    
+
     for key, value in d2.items():
         if key in merged:
             match (merged[key], value):
@@ -200,7 +204,3 @@ def deep_merge_dicts(d1: dict[str, Any], d2: dict[str, Any]) -> dict[str, Any] |
             merged[key] = value
 
     return merged
-
-
-class drop(ABC):
-    """Sentinel value to remove components"""
