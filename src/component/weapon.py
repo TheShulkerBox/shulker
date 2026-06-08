dfa9ebdf41abccf2c2efbd46b6ce951f6a5faf13
Thomas Hall <tallone155.6@gmail.com>
2026-06-07 18:58:09 -0700

    feat: Added weapon component with damage, speed, and weight fields

---
+++ src/component/weapon.py
@@ -0,0 +1,45 @@
+from typing import Any, Iterable
+
+from component.type import Component
+from lib.errors import CustomComponentError
+
+
+class Weapon(Component):
+    damage: float | None = None
+    speed: float | None = None
+    weight: str | None = None
+
+    def make_modifiers(self, type: str, value: float) -> Iterable[dict[str, Any]]:
+        yield {
+            "type": type,
+            "slot": "mainhand",
+            "id": "weapon.mainhand",
+            "amount": value,
+            "operation": "add_value",
+        }
+
+    def build(self) -> dict[str, Any]:
+        modifiers = []
+
+        if self.damage is not None:
+            modifiers.extend(self.make_modifiers("attack_damage", self.damage))
+
+        if self.speed is not None:
+            modifiers.extend(self.make_modifiers("attack_speed", self.speed))
+
+        if self.weight is not None:
+            if self.weight is "light":
+                ...
+            if self.weight is "medium":
+                ...
+            if self.weight is "heavy":
+                ...
+
+        if modifiers:
+            return {"attribute_modifiers": modifiers}
+
+        raise CustomComponentError(
+            "Need to define one of ['damage', 'speed', 'weight']",
+            "weapon",
+            self,
+        )
